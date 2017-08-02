a27265ed28cf9307f1786864319c515482a302f3
Guest User <Guest@epicodus-100.local>
2017-08-02 15:37:32 -0700

    this commit will add a empty repo

---
+++ Models/Hangman.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+class Hangman
+{
+    private string[] array = {"cat", "dog", "ball", "horse", "frog", "ant"};
+    int arrayLength = array.Length;
+    Random rnd = new Random();
+}
